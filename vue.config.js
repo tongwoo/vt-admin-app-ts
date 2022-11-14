ed0813f27375a4bfbeae280510c0f8ba390400f3
wutong <i@wutong.me>
2022-11-14 10:21:13 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
+const moment = require("moment")
 const {defineConfig} = require('@vue/cli-service')
+
 module.exports = defineConfig({
     publicPath: process.env.VUE_APP_PUBLIC_PATH,
     outputDir: 'app',
     transpileDependencies: true,
     lintOnSave: false,
+    chainWebpack(config) {
+        //在index.html中嵌入打包时间
+        config.plugin('html').tap((options) => {
+            options[0].publishTime = moment().format('YYYY-MM-DD HH:mm:ss')
+            return options
+        })
+    },
     devServer: {
         open: true,
         host: 'localhost',

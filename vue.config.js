e4e92cb471aa8964a2e048b7bff314577b2dde44
wutong <i@wutong.me>
2023-04-15 23:37:57 +0800

    修改结构

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     publicPath: process.env.VUE_APP_PUBLIC_PATH,
     outputDir: 'app',
     transpileDependencies: true,
+    productionSourceMap: false,
     lintOnSave: false,
     chainWebpack(config) {
         //不对图片进行转base64处理
@@ -20,9 +21,11 @@ module.exports = defineConfig({
         })
     },
     devServer: {
-        host: '0.0.0.0',
         //port: 50000,
-        allowedHosts: ['*'],
+        allowedHosts: ['all'],
+        client: {
+            progress: true,
+        },
         proxy: {
             //接口路径和映射地址，不要修改此处，酌情修改 .env.development 文件
             [process.env.VUE_APP_BASE_API]: {

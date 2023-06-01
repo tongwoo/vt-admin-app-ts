c463ef61b46eb1afa5c3ec3cc680557603aee526
wutong <i@wutong.me>
2023-06-01 17:46:48 +0800

    增加函数

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -5,3 +5,12 @@
 export function isEmpty(data: any): boolean {
     return data === '' || data === null || data === undefined
 }
+
+
+/**
+ * 是否是数值
+ * @param data 要检测的数据
+ */
+export function isNumeric(data: any): boolean {
+    return typeof data === 'number' || !isNaN(parseFloat(data))
+}

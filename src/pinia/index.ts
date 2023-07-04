40f9d5753d766ace6e4fb832249146bd79764c8c
wutong <i@wutong.me>
2023-07-04 15:33:22 +0800

    优化结构

--- src/pinia/index.ts
+++ src/pinia/index.ts
@@ -2,12 +2,25 @@ import {createPinia, PiniaPluginContext} from 'pinia'
 
 export const pinia = createPinia()
 
+//需要排除不恢复的state
+const excludes = [
+    {
+        storeId: 'app',
+        property: 'components',
+    }
+]
+
 /**
  * Pinia插件 - 持久化保存store到localStorage以及自动恢复
  */
 pinia.use((context: PiniaPluginContext) => {
     const backup = localStorage.getItem(context.store.$id)
     const data = backup ? JSON.parse(backup) : {}
+    excludes.forEach((item) => {
+        if (context.store.$id === item.storeId && data?.[item.property]) {
+            delete data[item.property]
+        }
+    })
     context.store.$subscribe((mutation, state) => {
         localStorage.setItem(mutation.storeId, JSON.stringify(state))
     })
@@ -15,6 +28,7 @@ pinia.use((context: PiniaPluginContext) => {
         ...data
     }
 })
+
 /**
  * Pinia插件 - 解决无法在setup方式中调用$reset
  */

a57e70a5b058c3883a3e3b473dcc5ec733e12726
wutong <i@wutong.me>
2023-07-21 16:27:31 +0800

    增加函数

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -13,3 +13,49 @@ export function isEmpty(data: any): boolean {
 export function isNumeric(data: any): boolean {
     return typeof data === 'number' || !isNaN(parseFloat(data))
 }
+
+/**
+ * 从数据集合中提取指定字段连续的相同时段数据并生成新的集合
+ * @param items 数据集合
+ * @param nameField 要提取的名称字段
+ * @param valueField 要提取的值字段
+ */
+export function mergeProps<T extends Record<string, any>>(items: T[], nameField: keyof T, valueField: keyof T) {
+    const segments: Array<T & { firstValue: T[typeof valueField], lastValue: T[typeof valueField] }> = []
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i]
+        const key = item[nameField]
+        const segment: (typeof segments)[number] = {
+            ...item,
+            firstValue: item[valueField],
+            lastValue: item[valueField]
+        }
+        segments.push(segment)
+        for (let j = i + 1; j < items.length; j++) {
+            if (key === items[j][nameField]) {
+                segment.lastValue = items[j][valueField]
+                i = j
+                continue
+            }
+            break
+        }
+    }
+    return segments
+}
+
+/**
+ * 查找叶子节点
+ * @param items 要查找的数据节点集合
+ * @param childKey 子节点子属性名
+ */
+export function findLeafNode<T extends { [key: string]: any }>(items: T[], childKey: string = 'children'): T | null {
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i]
+        if (item[childKey]?.length > 0) {
+            return findLeafNode(item[childKey], childKey)
+        } else {
+            return item
+        }
+    }
+    return null
+}
--- src/utils/electricity.ts
+++ src/utils/electricity.ts
@@ -3,8 +3,8 @@ import moment from 'moment'
 /**
  * 用电时段
  */
-interface ElectricitySegment {
-    //时段名称
+interface TimeSegment {
+    //时段名称 尖峰平谷
     name: string,
     //起始时间，格式：13:30
     start: string | null,
@@ -13,25 +13,26 @@ interface ElectricitySegment {
 }
 
 /**
- * 从数据集合中提取指定字段连续的相同时段数据并组成一个用电时段集合
+ * 从数据集合中提取指定字段连续的相同时段数据并生成一个时段集合
  * @param items 数据集合
  * @param nameField 要提取的名称字段
  * @param timeField 要提取的时间字段
+ * @param format 是否自动格式化成时间格式
  */
-export function parseElectricitySegments<T extends Record<string, any>>(items: T[], nameField: string, timeField: string) {
-    const segments: ElectricitySegment[] = []
+export function buildTimeSegments<T extends Record<string, any>>(items: T[], nameField: string, timeField: string, format: boolean = false): TimeSegment[] {
+    const segments: TimeSegment[] = []
     for (let i = 0; i < items.length; i++) {
         const item = items[i]
         const key = item[nameField]
-        const segment:ElectricitySegment = {
+        const segment: TimeSegment = {
             name: key,
-            start: moment(item[timeField]).format('HH:mm'),
+            start: format ? moment(item[timeField]).format('HH:mm') : item[timeField],
             stop: null
         }
         segments.push(segment)
         for (let j = i + 1; j < items.length; j++) {
             if (key === items[j][nameField]) {
-                segment.stop = moment(items[j][timeField]).format('HH:mm')
+                segment.stop = format ? moment(items[j][timeField]).format('HH:mm') : items[j][timeField]
                 i = j
                 continue
             }
@@ -39,4 +40,49 @@ export function parseElectricitySegments<T extends Record<string, any>>(items: T
         }
     }
     return segments
+}
+
+/**
+ * 时段位置
+ */
+interface TimeSegmentPosition {
+    //时段名称
+    name: string,
+    //起始与结束位置
+    range: [number, number]
+}
+
+/**
+ * 计算时段的配置
+ * @param segments 时段集合
+ */
+export function computeTimeSegmentPosition(segments: TimeSegment[]): TimeSegmentPosition[] {
+    //24小时的总分钟数
+    const minutes = 1440
+
+    /**
+     * 计算时间所占的百分比
+     */
+    const computePercent = (time: string): number => {
+        return timeToMinutes(time) / minutes * 100
+    }
+
+    /**
+     * 将时间字符串转换成分钟数
+     * @param time 时间，示例 08:30
+     */
+    const timeToMinutes = (time: string) => {
+        const [hour, minute] = [...time.split(':')]
+        return parseInt(hour) * 60 + parseInt(minute)
+    }
+
+    return segments.map((segment) => {
+        return {
+            name: segment.name,
+            range: [
+                computePercent(segment.start!),
+                computePercent(segment.stop!)
+            ]
+        }
+    })
 }

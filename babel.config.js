b104a72fe6c4a19e0c0b72fcf1b3a5b5c5db6257
wutong <i@wutong.me>
2023-09-13 13:54:47 +0800

    保留开发环境下的console输出

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,18 @@
-module.exports = {
-    presets: [
-        '@vue/cli-plugin-babel/preset'
-    ],
-    plugins: [
+const plugins = []
+if (process.env.NODE_ENV !== 'development') {
+    plugins.push(
         [
             'transform-remove-console',
             {
                 'exclude': ['error', 'warn']
             }
         ]
-    ]
+    )
+}
+
+module.exports = {
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+    plugins: plugins
 }
